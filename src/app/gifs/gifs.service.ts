1ea02ad344026fb06d015a999b4f95aa96e4d253
sepermo <96898079+sepermo@users.noreply.github.com>
2023-01-20 20:25:12 -0500

    primera actualizacion

--- src/app/gifs/gifs.service.ts
+++ src/app/gifs/gifs.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class GifsService {
-
+  private apiKey: string = 'FgMH6ZbnQR2NMSszhguEx6SIVaPpdFF3';
   private _historial:string[] =[];
 
   get historial(){
@@ -18,6 +18,11 @@ export class GifsService {
       this._historial.unshift(query);
     }
     this._historial = this._historial.splice(0,10);
+
+    fetch('api.giphy.com/v1/gifs/search?api_key=FgMH6ZbnQR2NMSszhguEx6SIVaPpdFF3&q=dragon ball z&limit=10')
+    .then(resp =>{
+      resp.json().then(data => console.log(data))
+    })
 
     console.log(this._historial);
   }
